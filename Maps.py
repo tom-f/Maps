8f36b79aeb331659c940f01991b5065f42c796fd
Tom Fawssett <tom.fawssett@gmail.com>
2013-03-18 17:19:07 +0000

    Applcation runner

--- Maps.py
+++ Maps.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+from maps import application 
+import sys
 
 def main():
     """
     Get the show on the road.
     """
     print "Starting"
+    app = application.Application()
 
 if __name__ == "__main__":
     main()
